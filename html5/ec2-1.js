932d16d0968a9f49a8022aa1b821a6fcfe39ea93
OlahAndrei <zurnac@gmail.com>
2018-10-15 13:49:53 +0300

    actualizare versiune

---
+++ html5/ec2-1.js
@@ -0,0 +1,27 @@
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.10.15.1";
+
+function citire()
+{
+	var _a= document.getElementById("id_a").value;
+	var _a= document.getElementById("id_a").value;
+	var _a= document.getElementById("id_a").value;
+	
+	var coeficienti = {a:a,b:b,c:c};
+	
+	return ;1
+}
+function rezolvare()
+{
+	
+}
+function afisare()
+{
+	
+}
+
+function rezolva()
+{
+	var coeficienti = citire();
+	var solutii = rezolvare(coeficienti);
+	afisare(solutii);
+}
--- html5/ec2.js
+++ html5/ec2.js
@@ -4,8 +4,8 @@ function rezolva()
 {
 	//citire
 	var a= document.getElementById("id_a").value;
-	var a= document.getElementById("id_a").value;
-	var a= document.getElementById("id_a").value;
+	var b= document.getElementById("id_b").value;
+	var c= document.getElementById("id_c").value;
 	//rezolvare
 	var delta= b*b-4*a*c;
 	var x1_re,x2_re,x1_im,x2_im;
