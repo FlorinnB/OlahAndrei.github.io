434a58ba13c3d0222f1222390892fbc310698939
OlahAndrei <zurnac@gmail.com>
2018-10-29 16:03:48 +0200

    Update html5/orientation.js

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,5 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.10.29.4
-";
+document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.10.29.4";
 window.addEventListener("deviceorientation",on_device_orientation)
 //----------------------------------------
 function on_device_orientation(e){
