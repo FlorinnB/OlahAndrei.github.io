e645e900f49836d6ee304c313ea8a1945c94b5a0
OlahAndrei <zurnac@gmail.com>
2018-10-29 15:59:40 +0200

    desenare

--- html5/orientation.js
+++ html5/orientation.js
@@ -5,4 +5,15 @@ function on_device_orientation(e){
 	document.getElementById("id_alpha").innerHTML = e.alpha;
 	document.getElementById("id_beta").innerHTML = e.beta;
 	document.getElementById("id_gamma").innerHTML = e.gamma;
+	
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	
+		context.clearRect(0,0, canvas.width, canvas.height);
+	var R=10;
+	context.beginPath();
+	context.arc(canvas.width / 2 + e.gamma/90(canvas.width / 2-R),
+	            canvas.width / 2 + e.gamma/90(canvas.width / 2-R),
+	            R,0,2*Math.PI);
+	context.strike();
 }
