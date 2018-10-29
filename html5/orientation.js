ba6d9e47123b06f6fce736adb6dc3956bd3c7909
OlahAndrei <zurnac@gmail.com>
2018-10-29 15:29:36 +0200

    orientation

---
+++ html5/orientation.js
@@ -0,0 +1,8 @@
+document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.10.29.3";
+window.addEventListener("deviceorientation",on_device_orientation)
+//----------------------------------------
+function on_device_orientation(e){
+	document.getElementById("id_alpha").innerHTML = e.alpha;
+	document.getElementById("id_beta").innerHTML = e.beta;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
+}
