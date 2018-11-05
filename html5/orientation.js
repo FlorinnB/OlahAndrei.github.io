a42c2bcab654c28cdc51d9cc42c308ce6ab0dd4e
OlahAndrei <zurnac@gmail.com>
2018-11-05 13:22:45 +0200

    acc

--- html5/orientation.js
+++ html5/orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.10.29.5";
+document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.11.05.0";
 window.addEventListener("deviceorientation",on_device_orientation)
 //----------------------------------------
 function on_device_orientation(e){
@@ -16,4 +16,10 @@ function on_device_orientation(e){
 	            canvas.width / 2 + e.gamma/90*(canvas.width / 2-R),
 	            R,0,2*Math.PI);
 	context.strike();
+	
+}
+function on_device_motion(e) {
+	document.getElementById("id_acc_z").innerHTML = e.accelerationIncludingGravity.z;
+	document.getElementById("id_acc_x").innerHTML = e.accelerationIncludingGravity.x;
+	document.getElementById("id_acc_y").innerHTML = e.accelerationIncludingGravity.y;
 }
