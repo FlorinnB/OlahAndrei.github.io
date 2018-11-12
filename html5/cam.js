82fd7609e257907da35359d189bb3f0aa5f957d6
OlahAndrei <zurnac@gmail.com>
2018-11-12 14:18:53 +0200

    cam

---
+++ html5/cam.js
@@ -0,0 +1,18 @@
+document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.11.12.0";
+document.getElementById("id_button").addEventListener("click, start");
+
+function on_success(e)
+{
+	document.getElementById("id_video").srcObjet = e;
+}
+//------------------------------
+function on_failure(e)
+{
+	alert("Cannot read camera");
+}
+//-----------------------------
+function start()
+{
+	var c = {audio: true, video = true};
+    navigator.mediaDvices.getUserMedia(c).then(on_success).catch(on_failure)	;
+}
