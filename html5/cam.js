cab3ada3d63c646b4cf7061748497d82f51b1157
OlahAndrei <zurnac@gmail.com>
2018-11-12 14:30:14 +0200

    Update html5/cam.js

--- html5/cam.js
+++ html5/cam.js
@@ -13,6 +13,6 @@ function on_failure(e)
 //-----------------------------
 function start()
 {
-	var c = {audio: true, video: {facingMode:"environment}};
+	var c = {audio: true, video: {facingMode:"environment"}};
     navigator.mediaDvices.getUserMedia(c).then(on_success).catch(on_failure)	;
 }
