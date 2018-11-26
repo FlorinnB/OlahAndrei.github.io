eb4ea8ffb393e2ba24d8516e4b200e36ea435f7a
OlahAndrei <zurnac@gmail.com>
2018-11-26 14:29:16 +0200

    prevent default

--- touch.js
+++ touch.js
@@ -21,6 +21,7 @@ function on_touch_start(e)
 //--------------------------------
 function on_touch_move(e)
 {
+	e.preventDefault();
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
