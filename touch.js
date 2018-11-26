d9885d10464ab2c3432b7eadd14558dbdce94591
OlahAndrei <zurnac@gmail.com>
2018-11-26 14:39:23 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,9 +1,10 @@
-document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.11.26.3";
+document.getElementById("id_logic_version").innerHTML = "Bussiness version: 2018.11.26.4";
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 var canvas_bounding_rect = canvas.getBoundingClientRect();
 
+var last_pos = {x: 0, y: 0};
 //-------------------------------
 function on_touch_start(e)
 {
@@ -16,6 +17,8 @@ function on_touch_start(e)
 					0, 2 * Math.PI);
 		)
 		context.stroke();
+		last_post.x = e.changedTouches[i].pageX;
+		last_post.y = e.changedTouches[i].pageY;
 	}
 }
 //--------------------------------
@@ -25,11 +28,16 @@ function on_touch_move(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
+		context.lineTo(e.changedTouches[i].pageX = canvas_bounding_rect.left,
+		               e.changedTouches[i].pageY = canvas_bounding_rect.top);
+					   
 		context.arc(e.changedTouches[i].pageX = canvas_bounding_rect.left,
 		            e.changedTouches[i].pageY = canvas_bounding_rect.top,
 					10,
 					0, 2 * Math.PI);
 		)
 		context.stroke();
+		
 	}
 }
